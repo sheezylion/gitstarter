df1d09b7db97793e1af0f7ca8c6717a2d7907333
Adesokan segun <sheezycouture@gmail.com>
2023-09-11 23:05:12 +0100

    new changes

--- slack.js
+++
@@ -1,9 +0,0 @@
-import { WebClient } from '@slack/web-api';
-
-// Read a token from the environment variables
-const token = 'xoxb-5525350307635-5859334007671-H6hlWljM6RmrDNWzbgALjkJG';
-
-// Initialize
-const web = new WebClient(token);
-
-console.log("web",web);
